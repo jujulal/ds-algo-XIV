4659547cfef797a877cf90ab53aa366b8deffdcf
Prayag Upd <prayag.upd@gmail.com>
2018-02-13 00:42:33 -0800

    fix finding connected islands with iterative way

--- algorithms/src/main/scala/search/CountConnectedGraphNodes.scala
+++ algorithms/src/main/scala/search/CountConnectedGraphNodes.scala
@@ -1,62 +1,72 @@
 package search
 
+import java.util
+
 import scala.collection.mutable.ArrayBuffer
 
 /**
+  * Easy
+  *
+  * https://leetcode.com/problems/max-area-of-island/description/
+  *
   * https://www.hackerrank.com/challenges/connected-cell-in-a-grid/problem
   */
 
 object CountConnectedGraphNodes {
 
   def isValid(rc: (Int, Int), rowSize: Int, colSize: Int): Boolean =
-    if (rc._1 < 0 || rc._2 < 0
-      || rc._1 > rowSize - 1 || rc._2 > colSize - 1) false
+    if (rc._1 < 0 || rc._2 < 0 || rc._1 > rowSize - 1 || rc._2 > colSize - 1) false
     else true
 
-  def findConnectedNodes(node: (Int, Int), rowSize: Int, colSize: Int): IndexedSeq[(Int, Int)] = {
+  def findNeighbourNodes(node: (Int, Int), rowSize: Int, colSize: Int): IndexedSeq[(Int, Int)] = {
 
-    val r = node._1
-    val c = node._2
+    val row = node._1
+    val col = node._2
 
-    IndexedSeq((c - 1, r),
-      (c - 1, r - 1),
-      (r - 1, c),
-      (r - 1, c + 1),
-      (r, c + 1),
-      (r + 1, c + 1),
-      (r + 1, c),
-      (r + 1, c - 1)).filter(isValid(_, rowSize, colSize))
-
-  }
-
-  def connectedNodes(matrix: Array[Array[Int]], currentNode: (Int, Int), connected: Int, visited: IndexedSeq[(Int, Int)]): Int = {
-
-    if (matrix(currentNode._1)(currentNode._2) == 0 || visited.contains(currentNode)) connected
-
-    else {
-      val nodes = findConnectedNodes(currentNode, matrix.length, matrix(0).length)
-
-      val sum = nodes.zipWithIndex.map { case (neighbourNode, index) =>
-        if (index == 0) connectedNodes(matrix, neighbourNode, connected, visited :+ currentNode)
-        else connectedNodes(matrix, neighbourNode, connected, (visited :+ currentNode) ++ findConnectedNodes(nodes(index - 1), matrix.length, matrix(0).length))
-      }.sum
-
-      1 + sum
-    }
+    IndexedSeq((row, col - 1), (row - 1, col - 1),
+      (row - 1, col), (row - 1, col + 1),
+      (row, col + 1), (row + 1, col + 1),
+      (row + 1, col), (row + 1, col - 1)).filter(isValid(_, rowSize, colSize))
 
   }
 
   def connected(matrix: Array[Array[Int]]): Int = {
 
-    val connectedNodess = new ArrayBuffer[Int]()
+    val visitedNodes = scala.collection.mutable.ArrayBuffer.empty[(Int, Int)]
+
+    var neighboursMap = scala.collection.mutable.Map.empty[(Int, Int), IndexedSeq[(Int, Int)]]
 
     for (r <- 0 until matrix.length) {
       for (c <- 0 until matrix(r).length) {
-        connectedNodess += connectedNodes(matrix, (0, 0), 0, IndexedSeq.empty)
+
+        if (!visitedNodes.contains((r, c))) {
+          visitedNodes += ((r, c))
+
+          if (matrix(r)(c) == 1) {
+            val neighboursQueue = new java.util.LinkedList[(Int, Int)]()
+            var connectedNodesForCurrentNode = scala.collection.mutable.ArrayBuffer((r, c))
+
+            findNeighbourNodes((r, c), matrix.length, matrix.head.length).foreach(neighboursQueue.add)
+
+            while (!neighboursQueue.isEmpty) {
+              val neighbour = neighboursQueue.poll()
+              if (!visitedNodes.contains(neighbour)) {
+                visitedNodes += ((neighbour._1, neighbour._2))
+                if (matrix(neighbour._1)(neighbour._2) == 1) {
+                  connectedNodesForCurrentNode += ((neighbour._1, neighbour._2))
+                  findNeighbourNodes((neighbour._1, neighbour._2), matrix.length, matrix.head.length).foreach(neighboursQueue.add)
+                }
+              }
+            }
+
+            neighboursMap += ((r, c) -> connectedNodesForCurrentNode)
+          }
+        }
       }
     }
 
-    println(connectedNodess)
-    connectedNodess.max
+    val sizes = neighboursMap.values.map(_.size)
+    if (sizes.isEmpty) 0
+    else sizes.max
   }
 }
--- algorithms/src/test/scala/search/CountConnectedGraphNodesSpec.scala
+++ algorithms/src/test/scala/search/CountConnectedGraphNodesSpec.scala
@@ -4,6 +4,34 @@ import org.scalatest.FunSuite
 
 class CountConnectedGraphNodesSpec extends FunSuite {
 
+  test("0") {
+
+    val matrix = Array(
+      Array(0)
+    )
+
+    assert(CountConnectedGraphNodes.connected(matrix) == 0)
+  }
+
+  test("1") {
+
+    val matrix = Array(
+      Array(1)
+    )
+
+    assert(CountConnectedGraphNodes.connected(matrix) == 1)
+  }
+
+  test("1 * 1") {
+
+    val matrix = Array(
+      Array(1),
+      Array(0)
+    )
+
+    assert(CountConnectedGraphNodes.connected(matrix) == 1)
+  }
+
   test("2 * 2") {
 
     val matrix = Array(
@@ -25,6 +53,17 @@ class CountConnectedGraphNodesSpec extends FunSuite {
     assert(CountConnectedGraphNodes.connected(matrix) == 4)
   }
 
+  test("3 * 3 but 1 in 0 col") {
+
+    val matrix = Array(
+      Array(1, 1, 0),
+      Array(0, 0, 1),
+      Array(1, 1, 1)
+    )
+
+    assert(CountConnectedGraphNodes.connected(matrix) == 6)
+  }
+
   test("4 * 4") {
 
     val matrix = Array(
@@ -35,5 +74,49 @@ class CountConnectedGraphNodesSpec extends FunSuite {
     )
 
     assert(CountConnectedGraphNodes.connected(matrix) == 5)
+  }
+
+  test("8 * 9") {
+
+    """0 1   0   0     0     0   X10 X1
+       0 1   1   0     0     X11 0   0
+       0 1   0   0     0     0   X9  0
+       1 0   0   0     X17   X12 X8 0
+       1 0   X26 X18   0     X13 X7 X2
+       0 0   X25 X19   0     0 X6 0
+       X29   X27 0     X2    0   X16 0 0 X3
+       0 0   X24 X21   0     X14 X5 X4
+       0 X28 X23 X22   X15   0 0 0""".stripMargin
+
+    val matrixStr =
+    """0 1 0 0 0 0 1 1
+       0 1 1 0 0 1 0 0
+       0 1 0 0 0 0 1 0
+       1 0 0 0 1 1 1 0
+       1 0 1 1 0 1 1 1
+       0 0 1 1 0 0 1 0
+       1 1 0 1 1 0 0 1
+       0 0 1 1 0 1 1 1
+       0 1 1 1 1 0 0 0""".stripMargin
+
+    val matrix: Array[Array[Int]] = matrixStr.split("\n").map(x => x.trim.split("\\s+").map(_.toInt))
+
+    assert(CountConnectedGraphNodes.connected(matrix) == 29)
+  }
+
+  test("test case#5 -  7 * 5") {
+
+    val matrixStr =
+      """1 1 1 0 1
+         0 0 1 0 0
+         1 1 0 1 0
+         0 1 1 0 0
+         0 0 0 0 0
+         0 1 0 0 0
+         0 0 1 1 0""".stripMargin
+
+    val matrix: Array[Array[Int]] = matrixStr.split("\n").map(x => x.trim.split("\\s+").map(_.toInt))
+
+    assert(CountConnectedGraphNodes.connected(matrix) == 9)
   }
 }
